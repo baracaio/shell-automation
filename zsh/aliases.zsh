307b87767f7bd79e41699ad7c0a7682108a4dfff
Caio Baracat <caio.baracat@mercadolivre.com>
2023-05-03 14:07:56 -0300

    WIP add some configuration for MeLi

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
 alias reload="source ~/.zshrc"
+
+##############################
+########  MELI  ##############
+##############################
+
+alias colinit="colima start --cpu 4 --memory 8 --mount-type 9p --arch aarch64"
+alias XO="cd ~/dev/meli/fury_exchange-office"
+alias PROVIDERS="cd ~/dev/meli/fury_exchange-office-providers"
+alias meli="cd ~/dev/meli"
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,3 +3,21 @@ export ZSH="$HOME/.oh-my-zsh"
 
 # STARSHIP
 export STARSHIP_CACHE=~/.config/starship/cache
+
+# GITHUB
+export GITHUB_TOKEN="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"
+
+# GOLANG
+export GOPATH=$(go env GOPATH)
+export GOVERSION=1.19.7
+export GOROOT="/usr/local/go"
+export GOMELI="${GOPATH}/src/github.com/mercadolibre"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+export GOPRIVATE="github.com/mercadolibre"
+export GO111MODULE=on
+
+export _GID=$(shell id -g)
+export _UID=$(shell id -u)
+
+# Fury
+export PATH="$PATH:$HOME/Library/Python/3.8/bin"
